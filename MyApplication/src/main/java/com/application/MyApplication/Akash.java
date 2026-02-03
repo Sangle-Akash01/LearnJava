a4666500b7bdd298c0621f75f9596d203f902bb8
Sangle-Akash01 <akash.essentials160@gmail.com>
2026-02-03 19:37:23 +0530

    Update Akash.java
    file changed by Akash

--- MyApplication/src/main/java/com/application/MyApplication/Akash.java
+++ MyApplication/src/main/java/com/application/MyApplication/Akash.java
@@ -2,10 +2,10 @@ package com.application.MyApplication;
 
 public class Akash {
   String getName (){
-      int num = 10;
+      int num = 11;
     String name =  "Akash";
     int id = 125422;
-      if(num == 10){
+      if(num == 11){
           return "Akash ";
 
       }
