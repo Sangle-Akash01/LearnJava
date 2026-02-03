2f276789720dee5cd8dcb8b2f942ea6257dfb253
Sangle-Akash01 <akash.essentials160@gmail.com>
2026-02-03 19:38:01 +0530

    Update Akash.java

--- MyApplication/src/main/java/com/application/MyApplication/Akash.java
+++ MyApplication/src/main/java/com/application/MyApplication/Akash.java
@@ -2,10 +2,10 @@ package com.application.MyApplication;
 
 public class Akash {
   String getName (){
-      int num = 11;
+      int num = 12;
     String name =  "Akash";
     int id = 125422;
-      if(num == 11){
+      if(num == 12){
           return "Akash ";
 
       }
